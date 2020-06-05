4f1ec9d5d9bdad4fb3c1924e2fb3767115c69510
jamesqjiang <jamesqjiang@tencent.com>
2020-06-05 12:53:28 +0800

    first commit

---
+++ ql/src/java/org/apache/hadoop/hive/ql/hooks/custom_utils/JamesMysqlUtil.java
@@ -0,0 +1,88 @@
+package org.apache.hadoop.hive.ql.hooks.custom_utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JamesMysqlUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(JamesMysqlUtil.class);
+
+
+    // MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/developer";
+
+    // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
+    //static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    //static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB?useSSL=false&serverTimezone=UTC";
+
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "developer";
+    static final String PASS = "developer";
+
+    public static void main(String[] args) {
+        String lineage = "lineage string";
+        insertLineageIntoMysql(lineage);
+    }
+
+    public static void insertLineageIntoMysql(String lineage) {
+        if (null == lineage || lineage.isEmpty()) {
+            return;
+        }
+
+        LOG.info(String.format("%s", lineage));
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            // 注册 JDBC 驱动
+            Class.forName(JDBC_DRIVER);
+
+            // 打开链接
+            LOG.info("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            String sql = "INSERT INTO hive_lineage_log(lineage_str) VALUES (?)";
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, lineage);
+
+            int resultSet = ps.executeUpdate();
+            if (resultSet > 0) {
+                //如果插入成功，则打印success
+                LOG.info("Insert Successfully !");
+            } else {
+                //如果插入失败，则打印Failure
+                LOG.info("Insert Failed !");
+            }
+
+            // 完成后关闭
+            ps.close();
+            conn.close();
+        } catch (SQLException se) {
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        } catch (Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException se2) {
+            }// 什么都不做
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+}
